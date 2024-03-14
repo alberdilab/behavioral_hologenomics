27d0aba1106700990974ee0721fe86e19bee4308
Antton Alberdi <anttonalberdi@gmail.com>
2024-03-14 05:06:42 +0100

    Updates

--- hmsc/exe_model1_250_10.sh
+++ hmsc/exe_model1_250_10.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=4                      # Run on 4 CPUs
 #SBATCH --mail-user=antton.alberdi@sund.ku.dk
 #SBATCH --mem=800gb                      # Job memory request
-#SBATCH --time=3306                       # In minutes
+#SBATCH --time=1200                       # In minutes
 
 # Activate conda environment
 module load mamba/1.3.1
